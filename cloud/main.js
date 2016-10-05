ae7a2ccac608128242e9dd300a92e62c6a27fc32
shauntc <shaun@shauntc.com>
2016-10-04 19:23:15 -0700

    replace old main.js with main.js provided by Sam with custom javascript to send notifications

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,42 @@
+/**
+ * Created by Sam on 2016-03-28.
+ */
+'use strict';
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('hello', (request, response) => {
+    response.success('Hi');
 });
+
+Parse.Cloud.define("sendPushNotification", function (request, response) {
+
+    // Create the push data
+    const data = {
+        alert: "A notification is happening right now."
+    };
+
+    // Create a query for all installations
+    const installationQuery = new Parse.Query(Parse.Installation);
+
+    // Send the push notification to all installations
+    sendPushNotification(installationQuery, data)
+        .then(() => response.success())
+        .catch(error => response.error(error));
+});
+
+function sendPushNotification(installationQuery, data) {
+    return new Promise((resolve, reject) => {
+        Parse.Push.send({
+                where: installationQuery,
+                data: data
+            }, {useMasterKey: true})
+            .then(result => {
+                // Push was successful
+                console.log(`sendPushNotification success ${result}`);
+                resolve(result);
+            })
+            .catch(error => {
+                console.log(`sendPushNotification error ${error.message}`);
+                reject(error);
+            });
+    });
+};
