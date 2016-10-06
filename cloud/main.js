f46d62ff2716a50a56f5561357014864ee2279f0
shauntc <shaun@shauntc.com>
2016-10-06 12:10:29 -0700

    Updated cloud fn

--- cloud/main.js
+++ cloud/main.js
@@ -7,36 +7,38 @@ Parse.Cloud.define('hello', (request, response) => {
     response.success('Hi');
 });
 
+// Parse.Cloud.define("sendPushNotification", function (request, response) {
+//
+//     // Create the push data
+//     const data = {
+//         alert: request.params.AlertText
+//     };
+//
+//     // Create a query for all installations
+//     const installationQuery = new Parse.Query(Parse.Installation);
+//
+//     // Send the push notification to all installations
+//     sendPushNotification(installationQuery, data)
+//         .then(() => response.success())
+//         .catch(error => response.error(error));
+// });
+
+//Sends a push notification with request: a dictionary with parameters AlertText and channel
+//channel contains the channel you want to push the notification to
 Parse.Cloud.define("sendPushNotification", function (request, response) {
 
-    // Create the push data
-    const data = {
-        alert: request.params.AlertText
-    };
-
-    // Create a query for all installations
-    const installationQuery = new Parse.Query(Parse.Installation);
-
-    // Send the push notification to all installations
-    sendPushNotification(installationQuery, data)
-        .then(() => response.success())
-        .catch(error => response.error(error));
-});
-
-Parse.Cloud.define("sendSilentNotification", function (request, response) {
-                   
                    // Create the push data
                    const data = {
-                   alert: request.params.channel
-//                   content-available:1
+                   alert: request.params.AlertText
+                   //content-available:1 //Makes it a silent notification
                    };
-                   
-                   
-                   
+
+
+
                    // Create a query for all installations
                    const installationQuery = new Parse.Query(Parse.Installation);
                    installationQuery.equalTo("channels",request.params.channel);
-                   
+
                    // Send the push notification to all installations
                    sendPushNotification(installationQuery, data)
                    .then(() => response.success())
